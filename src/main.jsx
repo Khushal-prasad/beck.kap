fccc8fb81c631b7d57c7ec410fe49677ca11eb3c
Khushal-prasad <jeetuprasad277@gmail.com>
2024-10-30 22:22:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { defineConfig } from 'vite';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
@@ -9,3 +10,7 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+export default defineConfig({
+  base: '/beck.kap/',
+});
